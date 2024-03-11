1831137e7218f15b3f1b6be2a644b5ecfb1b6d01
solivehong <610958259@qq.com>
2024-03-11 16:07:39 +0800

    add new student

--- build_sendblog.sh
+++ build_sendblog.sh
@@ -1,2 +1,3 @@
 mkdocs build
 rsync -avzrtP --info=progress2   -e "ssh -p 22" ./site/blog*  root@118.195.223.193:/usr/local/nginx/html
+rsync -avzrtP --info=progress2   -e "ssh -p 22" ./site/team*  root@118.195.223.193:/usr/local/nginx/html
